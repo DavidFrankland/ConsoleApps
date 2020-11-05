5de397b9b1f2be2dd238084296167f7fc8020399
David Frankland <david.frankland@bjss.com>
2020-11-05 21:27:36 +0000

    added StringFormat

---
+++ StringFormat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringFormat
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            int? i = null;
+            int? j = null;
+
+            var message = $"i: {i} " +
+                          $"j: {j} ";
+
+            Console.WriteLine(message);
+        }
+    }
+}
