6a6a1386746bb952cb2447c78bf4e3e531fb1ea3
David Frankland <david@dmf-software.co.uk>
2021-07-22 19:19:02 +0100

    better handling of missing values, added usage comments

--- ConfigTest/ConfigHelper.cs
+++ ConfigTest/ConfigHelper.cs
@@ -5,22 +5,43 @@ namespace ConfigTest
 {
     public static class ConfigHelper
     {
+        /// <summary>
+        /// Gets value from config file.
+        /// </summary>
+        /// <param name="key">The key of the desired config value.</param>
+        /// <returns>String value if the setting exists, null otherwise.</returns>
         public static string GetSetting(string key)
         {
-            var configValue = ConfigurationManager.AppSettings[key];
-            if (configValue == null) throw new Exception($"Config setting not found: '{key}'");
-            return configValue;
+            return ConfigurationManager.AppSettings[key];
         }
 
+        /// <summary>
+        /// Gets value from config file.
+        /// </summary>
+        /// <typeparam name="T">The desired type of the returned value.</typeparam>
+        /// <param name="key">The key of the desired config value.</param>
+        /// <returns>A value of type <typeparamref name="T"/> if the setting exists.</returns>
+        /// <exception cref="Exception">description</exception>
         public static T GetSetting<T>(string key) where T : IConvertible
         {
-            return ConvertValue<T>(key, GetSetting(key));
+            var configValue = ConfigurationManager.AppSettings[key];
+            return configValue != null ? ConvertValue<T>(key, configValue) : throw new Exception($"Config setting not found: '{key}'");
         }
 
+        /// <summary>
+        /// Gets value from config file.
+        /// </summary>
+        /// <typeparam name="T">The desired type of the returned value.</typeparam>
+        /// <param name="key">The key of the desired config value.</param>
+        /// <param name="defaultValue">The value to return if the setting does not exist.</param>
+        /// <returns>
+        ///     A value of type <typeparamref name="T"/> if the setting exists and can be converted into a <typeparamref name="T"/>,
+        ///     <paramref name="defaultValue"/> if it doesn't exist or can't be converted.
+        /// </returns> 
         public static T GetSetting<T>(string key, T defaultValue) where T : IConvertible
         {
             var configValue = ConfigurationManager.AppSettings[key];
-            return configValue == null ? defaultValue : ConvertValue<T>(key, configValue);
+            return configValue != null ? ConvertValue<T>(key, configValue) : defaultValue;
         }
 
         private static T ConvertValue<T>(string key, string value) 
