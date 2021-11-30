ffa753f811fa3e39c3ca08e471572850cfb94629
David Frankland <david@dmf-software.co.uk>
2021-11-30 16:02:18 +0000

    added DateTimeFormats

---
+++ DateTimeFormats/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DateTimeFormats
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+
+            var dateTime = Convert.ToDateTime("2020-06-08T12:00:00.000Z");
+
+            var settings = new JsonSerializerSettings
+            {
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                DateFormatString = @"yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffK"
+            };
+
+            var jsonObject = JsonConvert.SerializeObject(dateTime, settings);
+            Console.WriteLine(jsonObject);
+        }
+    }
+}
